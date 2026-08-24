69443aab1e422ad4b9b51c2c58cdfda37e144d41
mavhenry-create <mavhenry0@gmail.com>
2026-08-24 14:31:35 -0600

    Initial commit

---
+++ login/auth/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="flex justify-center items-center">
+    <header className="header">
+      <h1 className="text-2xl">Worldle!</h1>
+    </header>
+    <main className="main">
+      
+    </main>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ login/auth/src/components/nav.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <>
+        <nav className="bg-gray-800 p-4">
+            <h1 className="text-white text-2xl font-bold">Worldle!</h1>
+            
+
+        </nav>
+        </>
+    )
+}
